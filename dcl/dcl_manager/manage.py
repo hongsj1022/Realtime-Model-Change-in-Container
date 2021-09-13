617acdf25a2449ad22b57b4a624644b7dbaeb5bd
hongsj1022 <hongsj1022@naver.com>
2021-09-13 16:01:24 +0900

    change log

--- dcl/dcl_manager/manage.py
+++ dcl/dcl_manager/manage.py
@@ -1,5 +1,6 @@
 # usage: python3 manage.py --cid <container_name> --model_dir <model_directory>
 
+import os
 import requests
 import argparse
 import shutil
@@ -32,11 +33,10 @@ def upper_dir(container_name):
                 overlay = "/var/lib/docker/overlay2/2921a7e1349b91a06253b29ce797d4331503925ace9f06b143d4aea49ea89f58/diff"
 	else:
 		names = [container.name for container in running_container]
-		print(names)
 
 		for name in names:
 			if name == container_name:
-				container_id = name.id
+				container_id = container.id
 	
 		overlay = client.inspect_container(container_id)['GraphDriver']['Data']['UpperDir']
 	
@@ -45,21 +45,20 @@ def upper_dir(container_name):
 if __name__ == "__main__":
 	
 	#Get parameters
-	cid, model = args()
+	cid, new_model = args()
 	
 	#Get container storage driver to change model
-	overlay = upper_dir(cid) + "/home/dcl_server"
-	old_model = overlay + model
+	overlay = upper_dir(cid) + "/home/dcl_server/can_detection/"
+	old_model = overlay + new_model
 
 	#Change model
 	#print(datetime.datetime.now())
-	old_model_size = os.stat(old_model).st_size
-	new_model_size = os.stat(model).st_size
+	old_model_size = str(os.stat(old_model).st_size)
+	new_model_size = str(os.stat(new_model).st_size)
 	
-	os.remove(old_model)
-	shutil.copy(model, old_model)
-
-	print(old_model + "(" + old_model_size + "bytes) is updated to " + new_model + "(" + new_model_size + "bytes) in container " + cid + " - " + datetime.datetime.now())
+	#os.remove(old_model)
+	shutil.copy(new_model, old_model)
+	print("Old_model(" + old_model_size + " bytes)" + " is updated to " + "New_model(" + new_model_size + " bytes)" + " in container " + cid)
 	
 	url = "http://localhost:8080/test"
 	#res = requests.get(url)
