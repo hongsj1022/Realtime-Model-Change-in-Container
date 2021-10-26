f106d4e1f5301da305e3bea3a82b42f086ea2bf5
hongsj1022 <hongsj1022@naver.com>
2021-10-26 12:22:28 +0900

    Update class

--- dcl/dcl_manager/manage.py
+++ dcl/dcl_manager/manage.py
@@ -2,6 +2,8 @@
 
 import logging
 import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 # Define logger
 
@@ -22,62 +24,101 @@ import argparse
 import shutil
 import docker
 import datetime
+import redis
+
+class Target:
+
+    watchDir = "/home/pi/dcl/dcl_manager/models"
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchDir, recursive=True)
+        self.observer.start()
+        
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+        self.observer.join()
+
+class DCL_Manager:
+
+    def __init__(self):
+        self.cid, self.model = args()
+        self.rd = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+    def upper_dir(self):
+        dockerClient = docker.from_env()
+        client = docker.APIClient()
+        
+        running_container = dockerClient.containers.list()
+        
+        if not running_container:
+            print("No running container")
+            exit()
+            
+        else:
+            names = [container.name for container in running_container]
+            
+            for name in names:
+                if name == self.cid:
+                    container_id = container.id
+                    
+            overlay = client.inspect_container(container_id)['GraphDriver']['Data']['UpperDir']
+        
+        return overlay
+
+
+    def model_discovery(self):
+        model_dir = self.rd.get(self.model)        
+        
+        return model_dir
+
 
 def args():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--cid', type=str,
-			 	required=True,
-				help = 'input container id')
-	parser.add_argument('--model_dir', type=str,
- 				required=True,
-				help = 'input model dir')
-
-	args = parser.parse_args()
-	cid = args.cid
-	model = args.model_dir
-	
-	return cid, model
-
-def upper_dir(container_name):
-	dockerClient = docker.from_env()
-	client = docker.APIClient()
-	
-	running_container = dockerClient.containers.list()
-
-	if not running_container:
-                print("No running container")
-		exit()
-
-	else:
-		names = [container.name for container in running_container]
-
-		for name in names:
-			if name == container_name:
-				container_id = container.id
-	
-		overlay = client.inspect_container(container_id)['GraphDriver']['Data']['UpperDir']
-	
-	return overlay
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cid', type=str,
+            required=True,
+            help = 'input container id')
+    parser.add_argument('--model_dir', type=str,
+            required=True,
+            help = 'input model dir')
+    
+    args = parser.parse_args()
+    cid = args.cid
+    model = args.model_dir
+    
+    return cid, model
 
 if __name__ == "__main__":
-	
-	#Get parameters
-	cid, new_model = args()
-	
-	#Get container storage driver to change model
-	overlay = upper_dir(cid)
-	old_model = overlay + "/home/dcl_server/can_detection/checkpoints_cafe7/tiny1_4000.pth"
 
-	#Change model
-	#print(datetime.datetime.now())
-	old_model_size = str(os.stat(old_model).st_size)
-	new_model_size = str(os.stat(new_model).st_size)
-	
-	os.remove(old_model)
-	shutil.copy(new_model, old_model)
-	print("OLD model(" + old_model_size + " bytes)" + " is updated to " + "NEW model(" + new_model_size + " bytes)" + " in container " + cid)
-        #print("updated to " + "NEW model(" + new_model_size + " bytes)" + " in container " + cid)
-	
-	url = "http://localhost:8080/can"
-	res = requests.get(url)
-	print(res.status_code)
+    #How can DCL_Manager recognize that model copied in directory???
+    
+    #Get parameters
+    cid, new_model = args()
+    
+    #Get container storage driver to change model
+    overlay = upper_dir(cid)
+    
+    #Access redis
+    rd = redis.StrictRedis(host="localhost", port=6379, db=0)
+    
+    #Discover old model directory
+    old_model = overlay + model_discovery(rd, new_model)
+    
+    #Change model
+    old_model_size = str(os.stat(old_model).st_size)
+    new_model_size = str(os.stat(new_model).st_size)
+    
+    os.remove(old_model)
+    shutil.copy(new_model, old_model)
+    print("OLD model(" + old_model_size + " bytes)" + " is updated to " + "NEW model(" + new_model_size + " bytes)" + " in container " + cid)
+    
+    url = "http://localhost:8080/can"
+    res = requests.get(url)
+    print(res.status_code)
